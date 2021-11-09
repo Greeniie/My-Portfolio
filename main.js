8be72fb688a53644eb715d04f456e2be3851d587
Teejayyy <sirteejay007@gmail.com>
2021-11-09 17:45:34 +0100

    new commit

--- main.js
+++ main.js
@@ -10,28 +10,27 @@ const nightModeToggler = document.querySelector(".modeToggle")
 
 
 const mobileMenu = () => {
-if (hamburger.classList.contains("active") )
-   {
-    banner.classList.remove('blurry');
-    body.classList.remove('noscroll');
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-} else {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-    body.classList.toggle('noscroll');
-    banner.classList.toggle('blurry');
-}
+    if (hamburger.classList.contains("active")) {
+        banner.classList.remove('blurry');
+        body.classList.remove('noscroll');
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+    } else {
+        hamburger.classList.toggle("active");
+        navMenu.classList.toggle("active");
+        body.classList.toggle('noscroll');
+        banner.classList.toggle('blurry');
+    }
 
 }
 
-// hamburger.addEventListener("click", mobileMenu);
+hamburger.addEventListener("click", mobileMenu);
 
 
 
 // const lightMode = (e) => {
 //     e.preventDefault();
-    
+
 
 
 //  body.classList.toggle('light-mode')
